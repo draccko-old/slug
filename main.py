8f3bd20db5b324061e074d70aa877c7c06530254
dracckodev-laptop <dracckito@gmail.com>
2025-04-10 16:20:18 +0200

    Added some variables, changed some methods...

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import ultrasonic
 import figureCoords #type:ignore
 import exploration #type:ignore
+mps = 0
+explorating = True
 
 print(ultrasonic.measure_distance())
 print(figureCoords.get_point_coordinates(0, 1, 1, 90))
 
-
-explorating = True
-while explorating:
-    exploration.explore()
